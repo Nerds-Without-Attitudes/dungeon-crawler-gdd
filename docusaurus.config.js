89c831ba36b488c6f2086adfbb32c38801995059
Leonardo C.A.Rocha <leonardocar.dm@gmail.com>
2024-10-09 18:42:05 +0200

    Config files

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+export default {
+  // ...
+  url: 'nerds-without-attitudes.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'Untitled Dungeon Crawler',
+  organizationName: 'Nerds Without Attitudes',
+  trailingSlash: false,
+  // ...
+};
