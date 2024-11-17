d517157a356902ecc66ba932289dd640b2fb0192
Leonardo C.A.Rocha <leonardocar.dm@gmail.com>
2024-11-17 15:14:40 +0100

    Redirect to next docs instead of latest released in development mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,15 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   const introDocsUrl = useBaseUrl('/docs/intro');
+  const introDocsNextUrl = useBaseUrl('/docs/next/intro');
+  const development = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
   return (
     <Layout
       title={`Nerds Without Attitudes`}
       description="House for all details about the studio and games.">
       {/* Add header back once it make sense to have a proper landing page. <HomepageHeader /> */}
-      <Redirect to={introDocsUrl} />;
+      {development && <Redirect to={introDocsNextUrl} />}
+      {!development && <Redirect to={introDocsUrl} />}
     </Layout>
   );
 }
